259cfda20fab274bdf536c16084d8bdbc2246ef5
Alex Chien <alex.chien@koocaa.com>
2016-05-11 02:17:01 +0800

    add some chain param

--- lib/graphene.rb
+++ lib/graphene.rb
@@ -17,6 +17,7 @@ module Graphene
   autoload :Logger,     'graphene/logger'
   autoload :Key,        'graphene/key'
   autoload :Memo,       'graphene/memo'
+  autoload :Blockchain, 'graphene/blockchain'
   # autoload :Builder,    'graphene/builder'
 
   require 'graphene/util'
@@ -125,6 +126,8 @@ module Graphene
   NETWORKS = {
     graphene: {
       project: :graphene,
+      chain_id: "8796dc9de67e7a118bc89c4a9b42a24c5bbd68f3b7d185aea7250ce1ec485059",
+      core_symbol: "CORE",
       magic_head: "\xF9\xBE\xB4\xD9", #check
       address_version: "00",
       p2sh_version: "05",
@@ -158,55 +161,11 @@ module Graphene
 
   NETWORKS[:bitshares] = NETWORKS[:graphene].merge({
     project: :bitshares,
+    chain_id: "4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8",
+    core_symbol: "BTS",
     address_prefix: "BTS",
     genesis_hash: "4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8"
   })
 
-  module Blockchain
-    module Space
-      RELATIVE_PROTOCOL = 0
-      PROTOCOL          = 1
-      IMPLEMENTATION    = 2
-    end
 
-    module Apis
-        DATABASE           = "database"
-        NETWORK_BROADCAST  = "network_broadcast_api"
-        NETWORK_NODE       = "network_node_api"
-        HISTORY            = "history"
-    end
-
-    OBJECT_TYPE = {
-      "NULL"                             => "1.0.%d",
-      "BASE"                             => "1.1.%d",
-      "ACCOUNT"                          => "1.2.%d",
-      "ASSET"                            => "1.3.%d",
-      "FORCE_SETTLEMENT"                 => "1.4.%d",
-      "COMMITTEE_MEMBER"                 => "1.5.%d",
-      "WITNESS"                          => "1.6.%d",
-      "LIMIT_ORDER"                      => "1.7.%d",
-      "CALL_ORDER"                       => "1.8.%d",
-      "CUSTOM"                           => "1.9.%d",
-      "PROPOSAL"                         => "1.10.%d",
-      "OPERATION_HISTORY"                => "1.11.%d",
-      "WITHDRAW_PERMISSION"              => "1.12.%d",
-      "VESTING_BALANCE"                  => "1.13.%d",
-      "WORKER"                           => "1.14.%d",
-      "BALANCE"                          => "1.15.%d",
-      "GLOBAL_PROPERTY"                  => "2.0.%d",
-      "DYNAMIC_GLOBAL_PROPERTY"          => "2.1.%d",
-      "RESERVED0"                        => "2.2.%d",
-      "ASSET_DYNAMIC_DATA"               => "2.3.%d",
-      "ASSET_BITASSET_DATA"              => "2.4.%d",
-      "ACCOUNT_BALANCE"                  => "2.5.%d",
-      "ACCOUNT_STATISTICS"               => "2.6.%d",
-      "TRANSACTION"                      => "2.7.%d",
-      "BLOCK_SUMMARY"                    => "2.8.%d",
-      "ACCOUNT_TRANSACTION_HISTORY"      => "2.9.%d",
-      "BLINDED_BALANCE"                  => "2.10.%d",
-      "CHAIN_PROPERTY"                   => "2.11.%d",
-      "WITNESS_SCHEDULE"                 => "2.12.%d",
-      "BUDGET_RECORD"                    => "2.13.%d"
-    }
-  end
 end
---
+++ lib/graphene/blockchain.rb
@@ -0,0 +1,49 @@
+module Graphene
+  module Blockchain
+    module Space
+      RELATIVE_PROTOCOL = 0
+      PROTOCOL          = 1
+      IMPLEMENTATION    = 2
+    end
+
+    module Apis
+        DATABASE           = "database"
+        NETWORK_BROADCAST  = "network_broadcast_api"
+        NETWORK_NODE       = "network_node_api"
+        HISTORY            = "history"
+    end
+
+    OBJECT_TYPE = {
+      "NULL"                             => "1.0.%d",
+      "BASE"                             => "1.1.%d",
+      "ACCOUNT"                          => "1.2.%d",
+      "ASSET"                            => "1.3.%d",
+      "FORCE_SETTLEMENT"                 => "1.4.%d",
+      "COMMITTEE_MEMBER"                 => "1.5.%d",
+      "WITNESS"                          => "1.6.%d",
+      "LIMIT_ORDER"                      => "1.7.%d",
+      "CALL_ORDER"                       => "1.8.%d",
+      "CUSTOM"                           => "1.9.%d",
+      "PROPOSAL"                         => "1.10.%d",
+      "OPERATION_HISTORY"                => "1.11.%d",
+      "WITHDRAW_PERMISSION"              => "1.12.%d",
+      "VESTING_BALANCE"                  => "1.13.%d",
+      "WORKER"                           => "1.14.%d",
+      "BALANCE"                          => "1.15.%d",
+      "GLOBAL_PROPERTY"                  => "2.0.%d",
+      "DYNAMIC_GLOBAL_PROPERTY"          => "2.1.%d",
+      "RESERVED0"                        => "2.2.%d",
+      "ASSET_DYNAMIC_DATA"               => "2.3.%d",
+      "ASSET_BITASSET_DATA"              => "2.4.%d",
+      "ACCOUNT_BALANCE"                  => "2.5.%d",
+      "ACCOUNT_STATISTICS"               => "2.6.%d",
+      "TRANSACTION"                      => "2.7.%d",
+      "BLOCK_SUMMARY"                    => "2.8.%d",
+      "ACCOUNT_TRANSACTION_HISTORY"      => "2.9.%d",
+      "BLINDED_BALANCE"                  => "2.10.%d",
+      "CHAIN_PROPERTY"                   => "2.11.%d",
+      "WITNESS_SCHEDULE"                 => "2.12.%d",
+      "BUDGET_RECORD"                    => "2.13.%d"
+    }
+  end
+end
